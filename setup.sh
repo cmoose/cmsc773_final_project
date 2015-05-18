13181ce243e8cfd7c9f92b498337346492c32094
Chris Musialek <chris@musialek.org>
2015-05-18 12:51:40 -0400

    Adding a test for the reddit-control directory, needed for Philip to run our preprocessing code

--- setup.sh
+++ setup.sh
@@ -2,9 +2,15 @@
 
 #Insure project materials are in dir
 PROJECT_MATERIALS="project_materials"
+REDDIT_DIR="reddit-control"
 if [ ! -d "$PROJECT_MATERIALS" ]; then
   echo "ERROR!!"
   echo "copy and unzip the project_materials.zip file to this directory first, then rerun this"
+  exit 1
+fi
+if [ ! -d "$REDDIT_DIR" ]; then
+  echo "ERROR!!"
+  echo "copy and untar the reddit-for-project.tgz file to this directory first, then rerun this"
   exit 1
 fi
 
